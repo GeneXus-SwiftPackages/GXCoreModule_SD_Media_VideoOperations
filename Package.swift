2964584d3c6376e56149f8f71c6c0747d231b450
genexusbot <builder@genexus.com>
2023-03-09 12:49:00 -0300

    Package version: 1.0.0-beta+20230309154144

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Media_VideoOperations",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Media_VideoOperations-1.0.0-beta+20230309150554.xcframework.zip",
-			checksum: "cc9b805c3da3450d6360cd95a74268ad64201dd8cba8ec74726473286cce5b8b"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Media_VideoOperations-1.0.0-beta+20230309154144.xcframework.zip",
+			checksum: "ecd144c4876109c8793496c9c0c7f158a790653f63283183624f9457ec5dde31"
 		)
 	]
 )
