b863d6986f34bbb752e054e8f855d06ead0b5c1d
Build Er <builder@macmini6.local>
2023-05-24 15:56:16 -0300

    Package version: 1.0.0-beta+20230524184838

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
 		.target(name: "GXCoreModule_SD_Media_VideoOperationsWrapper",
 				dependencies: [
 					"GXCoreModule_SD_Media_VideoOperations",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.watchOS, .iOS, .tvOS]))
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .iOS, .watchOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Media_VideoOperations",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Media_VideoOperations-1.0.0-beta+20230519205149.xcframework.zip",
-			checksum: "8ed83f19717804d435406a1ce2fe747bceca51151c26912c31fd6b4f48d1f3fe"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Media_VideoOperations-1.0.0-beta+20230524184838.xcframework.zip",
+			checksum: "9462c47866038ea9f09919ead3d2eadc65cca4ee1238900d514a452ebfec83a5"
 		)
 	]
 )
