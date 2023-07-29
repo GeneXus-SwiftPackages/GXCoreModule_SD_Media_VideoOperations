18bbdfe3480c45b2056590498031440c7dce673c
genexusbot <builder@genexus.com>
2023-07-29 18:43:48 -0300

    Package version: 1.1.0-rc.1

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXCoreModule_SD_Media_VideoOperations",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXCoreModule_SD_Media_VideoOperations",
+			targets: ["GXCoreModule_SD_Media_VideoOperationsWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0-rc.1")
+	],
+	targets: [
+		.target(name: "GXCoreModule_SD_Media_VideoOperationsWrapper",
+				dependencies: [
+					"GXCoreModule_SD_Media_VideoOperations",
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.watchOS, .tvOS, .iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXCoreModule_SD_Media_VideoOperations",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_SD_Media_VideoOperations-1.1.0-rc.1.xcframework.zip",
+			checksum: "b083ef77f79a6062776ddb45cf136a5dfba67f6bd013fc62933d4398055f9438"
+		)
+	]
+)
