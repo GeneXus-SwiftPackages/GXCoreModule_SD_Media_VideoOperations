58ea7fc5376fda09cda027d27aecf20ffe31dab9
Build Er <builder@macmini6.local>
2024-05-08 12:47:41 -0300

    Package version: 2.0.0-beta.27

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_SD_Media_VideoOperationsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "2.0.0-beta.26")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "2.0.0-beta.27")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_Media_VideoOperationsWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Media_VideoOperations",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Media_VideoOperations-2.0.0-beta.26.xcframework.zip",
-			checksum: "9a6afa8410edd0e73343209fd7cc306b29a6d1a3cc6ed4a2158bab5ed6ca0b4a"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Media_VideoOperations-2.0.0-beta.27.xcframework.zip",
+			checksum: "453de80aa05c1745422cfeeaac822b49a274ee396575d9a0cb196b0dfe4ce2b7"
 		)
 	]
 )
