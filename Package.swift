f42f380d33c9bb9899e0b13055191149f1c1d55b
The iOS Team @ GeneXus <builder@genexus.com>
2024-11-26 12:04:35 -0300

    Package version: 2.2.0-beta.15

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_SD_Media_VideoOperationsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "2.2.0-beta.14")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "2.2.0-beta.15")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_Media_VideoOperationsWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Media_VideoOperations",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Media_VideoOperations-2.2.0-beta.14.xcframework.zip",
-			checksum: "d18712b55d09262f485126fcd53ad0782cb613fa0c7b5655c2d33817697e14d5"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Media_VideoOperations-2.2.0-beta.15.xcframework.zip",
+			checksum: "5d72191c92a9eadb618c49a38464b442059148be658f6bdfa29afe70322fcb03"
 		)
 	]
 )
